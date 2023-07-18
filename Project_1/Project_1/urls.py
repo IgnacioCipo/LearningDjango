d631cb106cdd54297d5293697281fc5eab474846
IgnacioCipo <ignaciojcipolatti@gmail.com>
2023-07-17 23:07:35 -0300

    First view Hello World!

--- Project_1/Project_1/urls.py
+++ Project_1/Project_1/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from Project_1.views import greetings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('greetings', greetings),
 ]
