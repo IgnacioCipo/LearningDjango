d20b1ce30138555fc1391e60e505ec0686bb83b2
IgnacioCipo <ignaciojcipolatti@gmail.com>
2023-08-01 17:07:01 -0300

    Printing elements of a list in template

--- Project_1/Project_1/views.py
+++ Project_1/Project_1/views.py
@@ -2,13 +2,19 @@ from django.http import HttpResponse
 import datetime 
 from django.template import Template, Context
 
+class Person():
+    def __init__(self, name, lastname):
+        self.name = name
+        self.lastname = lastname
+
 def greetings(request):
-    name = "Ignacio"
-    lastname = "Cipolatti"
+    person_1 = Person("Ignacio", "Cipolatti")
+    my_themes = ["Templates", "Models", "Forms", "Views", "Deploy"]
+    time = datetime.datetime.now()
     extern_doc = open("/home/cipo/Documents/LearningDjango/Project_1/Project_1/templates/my_template.html")
     template = Template(extern_doc.read())
     extern_doc.close()
-    ctx = Context({"personal_name": name, "personal_lastname": lastname})
+    ctx = Context({"personal_name": person_1.name, "personal_lastname": person_1.lastname, "current_time": time, "themes": my_themes})
     renderized_doc = template.render(ctx)
     return HttpResponse(renderized_doc)
 
